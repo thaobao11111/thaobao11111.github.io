e678fca03c074cf931decbe9119c9458ceece4fc
Thai Bao <thaobao11111@gmail.com>
2024-11-19 01:29:50 +0700

    Updated repo with Release, Packages and Packages.gz

--- c.py
+++ c.py
@@ -1,43 +1,35 @@
 import os
 import gzip
 
-# Bước 1: Tạo tệp Release
-release_content = """Origin: My Repo
-Label: My Repo
+# Đường dẫn tới thư mục repo của bạn
+repo_dir = "./dists/stable/main"
+
+# Tạo file Release
+release_content = """Origin: myrepo
+Label: myrepo
 Suite: stable
 Codename: stable
-Version: 1.0
-Architectures: iphoneos-arm
+Architectures: iphoneos-arm, iphoneos-arm64
 Components: main
-Description: My custom repo for iPhone
+Description: My Custom Repo for Jailbreak Apps
 """
+with open(os.path.join(repo_dir, "Release"), "w") as release_file:
+    release_file.write(release_content)
 
-# Lưu tệp Release
-with open('./dists/stable/Release', 'w') as f:
-    f.write(release_content)
+# Tạo file Packages
+packages_content = """Package: myapp
+Version: 1.0.0
+Architecture: iphoneos-arm
+Maintainer: me@myemail.com
+Depends: com.apple.later, libirecovery
+Description: My Custom App for Jailbreak
+"""
+with open(os.path.join(repo_dir, "Packages"), "w") as packages_file:
+    packages_file.write(packages_content)
 
-print("File Release đã được tạo thành công!")
-
-# Bước 2: Tạo tệp Packages
-deb_directory = './dists/stable/main/binary-iphoneos-arm'
-
-with open('./dists/stable/main/Packages', 'w') as f:
-    for deb_file in os.listdir(deb_directory):
-        if deb_file.endswith('.deb'):
-            # Lấy kích thước của gói .deb
-            file_size = os.path.getsize(os.path.join(deb_directory, deb_file))
-            f.write(f"Package: {deb_file}\n")
-            f.write(f"Architecture: iphoneos-arm\n")
-            f.write(f"Version: 1.0\n")
-            f.write(f"Filename: dists/stable/main/binary-iphoneos-arm/{deb_file}\n")
-            f.write(f"Size: {file_size}\n")
-            f.write(f"Description: My custom package\n\n")
-
-print("File Packages đã được tạo thành công!")
-
-# Bước 3: Nén tệp Packages thành Packages.gz
-with open('./dists/stable/main/Packages', 'rb') as f_in:
-    with gzip.open('./dists/stable/main/Packages.gz', 'wb') as f_out:
+# Nén file Packages thành Packages.gz
+with open(os.path.join(repo_dir, "Packages"), "rb") as f_in:
+    with gzip.open(os.path.join(repo_dir, "Packages.gz"), "wb") as f_out:
         f_out.writelines(f_in)
 
-print("File Packages.gz đã được tạo thành công!")
+print("Repo files (Release, Packages, Packages.gz) have been created and compressed successfully.")
